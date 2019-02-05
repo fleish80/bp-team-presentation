b5bb5bef8081ad8e979af3e24dadf1b2706b03d0
fleish80 <Fleish.D@gmail.com>
2019-02-05 18:07:30 +0200

    form control

--- src/app/reactive-form/form-control/form-control.component.ts
+++ src/app/reactive-form/form-control/form-control.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-form-control',
   templateUrl: './form-control.component.html',
   styleUrls: ['./form-control.component.scss']
 })
-export class FormControlComponent implements OnInit {
+export class FormControlComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+
+  formGroup: FormGroup;
+  nameCtrl: FormControl;
+  clientMessage = 'client message';
+  serverMessage: string;
+  private subscriptions = new Subscription();
+
+  constructor(private formBuilder: FormBuilder) {
+
+   }
 
   ngOnInit() {
+    this.nameCtrl = new FormControl();
+    this.subscriptions.add(
+    this.nameCtrl.valueChanges.subscribe((value: string) => {
+      if (value === 'server') {
+        this.serverMessage = 'server message';
+      } else {
+        this.serverMessage = '';
+      }
+    }));
+    this.formGroup = this.formBuilder.group({
+      name: this.nameCtrl
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 }
--- src/app/reactive-form/reactive-form.module.ts
+++ src/app/reactive-form/reactive-form.module.ts
@@ -4,12 +4,15 @@ import { ReactiveFormRoutingModule } from './reactive-form-routing.module';
 import { FormControlComponent } from './form-control/form-control.component';
 import { FormControlNameComponent } from './form-control-name/form-control-name.component';
 import { ReactiveFormComponent } from './reactive-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FormControlComponent, FormControlNameComponent, ReactiveFormComponent],
   imports: [
     CommonModule,
-    ReactiveFormRoutingModule
+    ReactiveFormRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ReactiveFormModule { }
