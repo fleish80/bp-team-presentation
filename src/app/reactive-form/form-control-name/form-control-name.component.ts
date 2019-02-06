9580f3d821e4d86202b6fc1f129c70d4402504d9
fleish80 <Fleish.D@gmail.com>
2019-02-06 17:21:21 +0200

    form control name

--- src/app/reactive-form/form-control-name/form-control-name.component.ts
+++ src/app/reactive-form/form-control-name/form-control-name.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-form-control-name',
   templateUrl: './form-control-name.component.html',
   styleUrls: ['./form-control-name.component.scss']
 })
-export class FormControlNameComponent implements OnInit {
+export class FormControlNameComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  formGroup: FormGroup;
+  clientMessage = 'client message';
+  serverMessage: string;
+  private subscriptions = new Subscription();
+
+  constructor(private formBuilder: FormBuilder) {
+
+   }
 
   ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+      name: []
+    });
+    this.subscriptions.add(
+      this.formGroup.get('name').valueChanges.subscribe((value: string) => {
+        if (value === 'server') {
+          this.serverMessage = 'server message';
+        } else {
+          this.serverMessage = '';
+        }
+      }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
 }
